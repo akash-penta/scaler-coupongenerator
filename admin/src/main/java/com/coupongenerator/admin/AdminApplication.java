c6249b7703d2d31c54e6bece64e53b4ff9703b88
akash-penta <akashpenta8@gmail.com>
2024-09-24 16:19:52 +0530

    Initial setup of admin service

---
+++ admin/src/main/java/com/coupongenerator/admin/AdminApplication.java
@@ -0,0 +1,14 @@
+package com.coupongenerator.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+		System.out.println("Admin started...");
+	}
+
+}
