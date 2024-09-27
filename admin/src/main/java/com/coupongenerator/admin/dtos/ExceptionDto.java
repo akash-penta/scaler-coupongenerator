48cababb5da84d5e0619ff78aa441aff83004f58
akash-penta <akashpenta8@gmail.com>
2024-09-27 15:51:46 +0530

    Added validation error message attribute

--- admin/src/main/java/com/coupongenerator/admin/dtos/ExceptionDto.java
+++ admin/src/main/java/com/coupongenerator/admin/dtos/ExceptionDto.java
@@ -1,12 +1,27 @@
 package com.coupongenerator.admin.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import org.springframework.http.HttpStatus;
+
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionDto {
     private int statusCode;
     private String message;
+    private Map<String, String> validationErrorMessages;
+
+    public ExceptionDto(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public ExceptionDto(int statusCode, Map<String, String> validationErrorMessages) {
+        this.statusCode = statusCode;
+        this.validationErrorMessages = validationErrorMessages;
+    }
 }
