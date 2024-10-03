4b8ab8a30f1e9885d805c2e804448845970ff0af
akash-penta <akashpenta8@gmail.com>
2024-10-03 14:27:54 +0530

    Changed API path

--- admin/src/main/java/com/coupongenerator/admin/controllers/PlanDetailsController.java
+++ admin/src/main/java/com/coupongenerator/admin/controllers/PlanDetailsController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/plan")
+@RequestMapping("/plan")
 public class PlanDetailsController {
 
     @Autowired
