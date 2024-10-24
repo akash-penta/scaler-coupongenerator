9d2101c4a56c59c58aef0178a5407cbecfa56fe2
akash-penta <akashpenta8@gmail.com>
2024-10-24 19:59:51 +0530

    Implemented validations for AuthController

--- admin/src/main/java/com/coupongenerator/admin/controllers/AuthController.java
+++ admin/src/main/java/com/coupongenerator/admin/controllers/AuthController.java
@@ -1,16 +1,26 @@
 package com.coupongenerator.admin.controllers;
 
 import com.coupongenerator.admin.authservices.JwtService;
+import com.coupongenerator.admin.dtos.ExceptionDto;
 import com.coupongenerator.admin.dtos.LoginRequestDto;
 import com.coupongenerator.admin.dtos.LoginResponseDto;
 import com.coupongenerator.admin.entities.AdminDetails;
 import com.coupongenerator.admin.services.AuthenticationService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/auth")
@@ -31,13 +41,40 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticate(
-            @RequestBody LoginRequestDto requestDto
+            @Valid @RequestBody LoginRequestDto requestDto,
+            BindingResult bindingResult
     ) {
+        if(bindingResult.hasErrors()) {
+            Set<String> errorSet = new HashSet<>();
+            Map<String, String> errorMap = new HashMap<>();
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError fieldError = (FieldError) objectError;
+                errorSet.add(fieldError.getDefaultMessage());
+            });
+
+            if(errorSet.contains("User name is mandatory")) {
+                errorMap.put("userName", "User name is mandatory");
+            }
+            else if(errorSet.contains("User name should not be blank")) {
+                errorMap.put("userName", "User name should not be blank");
+            }
+
+            if(errorSet.contains("Password is mandatory")) {
+                errorMap.put("password", "Password is mandatory");
+            }
+            else if(errorSet.contains("Password should not be blank")) {
+                errorMap.put("password", "Password should not be blank");
+            }
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ExceptionDto(HttpStatus.BAD_REQUEST.value(), errorMap)
+            );
+        }
         AdminDetails authenticatedUser = authenticationService.authenticate(requestDto);
 
         String jwtToken = jwtService.generateToken(authenticatedUser);
 
-//        LoginResponse loginResponse = new LoginResponse().setToken(jwtToken).setExpiresIn(jwtService.getExpirationTime());
         LoginResponseDto responseDto = new LoginResponseDto(jwtToken);
 
         return ResponseEntity.ok(responseDto);
--- admin/src/main/java/com/coupongenerator/admin/dtos/LoginRequestDto.java
+++ admin/src/main/java/com/coupongenerator/admin/dtos/LoginRequestDto.java
@@ -1,5 +1,7 @@
 package com.coupongenerator.admin.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequestDto {
+
+    @NotNull(message = "User name is mandatory")
+    @NotBlank(message = "User name should not be blank")
     private String userName;
+
+    @NotNull(message = "Password is mandatory")
+    @NotBlank(message = "Password should not be blank")
     private String password;
 }
