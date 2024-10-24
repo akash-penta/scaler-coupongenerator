c7bad802744e88215569775c23c89f37b20cc4df
akash-penta <akashpenta8@gmail.com>
2024-10-24 20:00:06 +0530

    Implemented validations for UserController

--- admin/src/main/java/com/coupongenerator/admin/controllers/UserController.java
+++ admin/src/main/java/com/coupongenerator/admin/controllers/UserController.java
@@ -1,19 +1,22 @@
 package com.coupongenerator.admin.controllers;
 
 import com.coupongenerator.admin.dtos.CreateUserRequestDto;
+import com.coupongenerator.admin.dtos.ExceptionDto;
 import com.coupongenerator.admin.dtos.UpdateUserRequestDto;
 import com.coupongenerator.admin.dtos.UserResponseDto;
 import com.coupongenerator.admin.exceptions.PlanNotFoundException;
 import com.coupongenerator.admin.exceptions.UserAlreadyExistsException;
 import com.coupongenerator.admin.exceptions.UserNotFoundException;
 import com.coupongenerator.admin.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @RequestMapping("/user")
@@ -24,8 +27,50 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<?> createUser(
-            @RequestBody CreateUserRequestDto requestDto
+            @Valid @RequestBody CreateUserRequestDto requestDto,
+            BindingResult bindingResult
     ) throws UserAlreadyExistsException, PlanNotFoundException {
+        if(bindingResult.hasErrors()) {
+            Set<String> errorSet = new HashSet<>();
+            Map<String, String> errorMap = new HashMap<>();
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError fieldError = (FieldError) objectError;
+                errorSet.add(fieldError.getDefaultMessage());
+            });
+
+            if(errorSet.contains("User name is mandatory")) {
+                errorMap.put("userName", "User name is mandatory");
+            }
+            else if(errorSet.contains("User name should not be blank")) {
+                errorMap.put("userName", "User name should not be blank");
+            }
+
+            if(errorSet.contains("Password is mandatory")) {
+                errorMap.put("password", "Password is mandatory");
+            }
+            else if(errorSet.contains("Password should not be blank")) {
+                errorMap.put("password", "Password should not be blank");
+            }
+
+            if(errorSet.contains("Business name is mandatory")) {
+                errorMap.put("businessName", "Business name is mandatory");
+            }
+            else if(errorSet.contains("Business name should not be blank")) {
+                errorMap.put("businessName", "Business name should not be blank");
+            }
+
+            if(errorSet.contains("Plan name is mandatory")) {
+                errorMap.put("planName", "Plan name is mandatory");
+            }
+            else if(errorSet.contains("Plan name should not be blank")) {
+                errorMap.put("planName", "Plan name should not be blank");
+            }
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ExceptionDto(HttpStatus.BAD_REQUEST.value(), errorMap)
+            );
+        }
         UserResponseDto responseDto = userService.createUser(requestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
@@ -42,6 +87,11 @@ public class UserController {
     public ResponseEntity<?> getUser(
             @PathVariable String userName
     ) throws UserNotFoundException {
+        if(userName.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ExceptionDto(HttpStatus.BAD_REQUEST.value(), "User Name should not be blank")
+            );
+        }
         UserResponseDto responseDto = userService.getUser(userName);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
@@ -49,19 +99,45 @@ public class UserController {
 
     @PatchMapping("/{id}")
     public ResponseEntity<?> updateUser(
-            @PathVariable UUID id,
+            @PathVariable String id,
             @RequestBody UpdateUserRequestDto requestDto
     ) throws UserNotFoundException, UserAlreadyExistsException, PlanNotFoundException {
-        userService.updateUser(id, requestDto);
+        if(id.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ExceptionDto(HttpStatus.BAD_REQUEST.value(), "Id should not be blank")
+            );
+        }
+        UUID uuid = null;
+        try {
+            uuid = UUID.fromString(id);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ExceptionDto(HttpStatus.BAD_REQUEST.value(), "Invalid id")
+            );
+        }
+        userService.updateUser(uuid, requestDto);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(
-            @PathVariable UUID id
+            @PathVariable String id
     ) throws UserNotFoundException {
-        userService.deleteUser(id);
+        if(id.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ExceptionDto(HttpStatus.BAD_REQUEST.value(), "Id should not be blank")
+            );
+        }
+        UUID uuid = null;
+        try {
+            uuid = UUID.fromString(id);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ExceptionDto(HttpStatus.BAD_REQUEST.value(), "Invalid id")
+            );
+        }
+        userService.deleteUser(uuid);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
     }
--- admin/src/main/java/com/coupongenerator/admin/dtos/CreateUserRequestDto.java
+++ admin/src/main/java/com/coupongenerator/admin/dtos/CreateUserRequestDto.java
@@ -1,24 +1,30 @@
 package com.coupongenerator.admin.dtos;
 
-import com.coupongenerator.admin.entities.PlanDetails;
-import com.coupongenerator.admin.enums.UserStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateUserRequestDto {
 
+    @NotNull(message = "User name is mandatory")
+    @NotBlank(message = "User name should not be blank")
     private String userName;
 
+    @NotNull(message = "Password is mandatory")
+    @NotBlank(message = "Password should not be blank")
     private String password;
 
+    @NotNull(message = "Business name is mandatory")
+    @NotBlank(message = "Business name should not be blank")
     private String businessName;
 
+    @NotNull(message = "Plan name is mandatory")
+    @NotBlank(message = "Plan name should not be blank")
     private String planName;
 
 }
