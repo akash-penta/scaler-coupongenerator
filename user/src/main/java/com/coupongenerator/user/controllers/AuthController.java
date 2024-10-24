6a82cc0307327e1f1671f1959cc24bc8479a724e
akash-penta <akashpenta8@gmail.com>
2024-10-24 20:03:36 +0530

    Implemented validations for AuthController

--- user/src/main/java/com/coupongenerator/user/controllers/AuthController.java
+++ user/src/main/java/com/coupongenerator/user/controllers/AuthController.java
@@ -1,18 +1,28 @@
 package com.coupongenerator.user.controllers;
 
 import com.coupongenerator.user.authservices.JwtService;
+import com.coupongenerator.user.dtos.ExceptionDto;
 import com.coupongenerator.user.dtos.LoginResponseDto;
 import com.coupongenerator.user.dtos.LoginRequestDto;
 import com.coupongenerator.user.entities.User;
 import com.coupongenerator.user.exceptions.UnauthorizedOperation;
 import com.coupongenerator.user.exceptions.UserNotFoundException;
 import com.coupongenerator.user.services.AuthenticationService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/auth")
@@ -26,13 +36,40 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticate(
-            @RequestBody LoginRequestDto requestDto
+            @Valid @RequestBody LoginRequestDto requestDto,
+            BindingResult bindingResult
     ) throws UserNotFoundException, UnauthorizedOperation {
+        if(bindingResult.hasErrors()) {
+            Set<String> errorSet = new HashSet<>();
+            Map<String, String> errorMap = new HashMap<>();
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError fieldError = (FieldError) objectError;
+                errorSet.add(fieldError.getDefaultMessage());
+            });
+
+            if(errorSet.contains("User name is mandatory")) {
+                errorMap.put("userName", "User name is mandatory");
+            }
+            else if(errorSet.contains("User name should not be blank")) {
+                errorMap.put("userName", "User name should not be blank");
+            }
+
+            if(errorSet.contains("Password is mandatory")) {
+                errorMap.put("password", "Password is mandatory");
+            }
+            else if(errorSet.contains("Password should not be blank")) {
+                errorMap.put("password", "Password should not be blank");
+            }
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ExceptionDto(HttpStatus.BAD_REQUEST.value(), errorMap)
+            );
+        }
         User authenticatedUser = authenticationService.authenticate(requestDto);
 
         String jwtToken = jwtService.generateToken(authenticatedUser);
 
-//        LoginResponse loginResponse = new LoginResponse().setToken(jwtToken).setExpiresIn(jwtService.getExpirationTime());
         LoginResponseDto responseDto = new LoginResponseDto(jwtToken);
 
         return ResponseEntity.ok(responseDto);
